a1feb03a67c98d7608cb8c62749d6096ecac15b7
TahaLechgar <tahamr08@gmail.com>
2023-01-04 09:54:23 +0100

    create basic spring boot project

---
+++ src/main/java/com/veinsmoke/myrhbackend/MyRhBackendApplication.java
@@ -0,0 +1,12 @@
+package com.veinsmoke.myrhbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRhBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyRhBackendApplication.class, args);
+    }
+
+}
