cdd4df2451a4a82d2569bfb44cea12e15f1587cb
TahaLechgar <tahamr08@gmail.com>
2023-01-04 14:15:32 +0100

    Add a CustomUserDetailsService that suits our authentication mechanism

---
+++ src/main/java/com/veinsmoke/myrhbackend/security/CostumeUserDetailsService.java
@@ -0,0 +1,13 @@
+package com.veinsmoke.myrhbackend.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CostumeUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
