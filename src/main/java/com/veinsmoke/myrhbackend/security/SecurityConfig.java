3cff91383ed6ff1cbb7e449fb92ba3a439f02bb0
TahaLechgar <tahamr08@gmail.com>
2023-01-04 11:20:48 +0100

    Configure SpringSecurity FilterChain

---
+++ src/main/java/com/veinsmoke/myrhbackend/security/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.veinsmoke.myrhbackend.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests().requestMatchers("/login/**", "/register", "/", "/verify")
+                .permitAll()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/company/**").hasAnyAuthority("COMPANY")
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/client/**").hasAnyAuthority("CLIENT")
+                .anyRequest()
+                .authenticated();
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
