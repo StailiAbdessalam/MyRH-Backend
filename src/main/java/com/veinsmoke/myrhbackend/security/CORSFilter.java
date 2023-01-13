6af86b0fb0a701af0046ce25f5029af97fe41b3b
TahaLechgar <tahamr08@gmail.com>
2023-01-13 16:06:45 +0100

    add CORSFilter to handle CORS

---
+++ src/main/java/com/veinsmoke/myrhbackend/security/CORSFilter.java
@@ -0,0 +1,33 @@
+package com.veinsmoke.myrhbackend.security;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class CORSFilter extends OncePerRequestFilter {
+
+
+    @Override
+    public void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
