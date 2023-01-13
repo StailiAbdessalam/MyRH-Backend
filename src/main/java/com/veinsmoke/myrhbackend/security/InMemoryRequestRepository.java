5995bffe321df4041c274022733551f5d494b516
TahaLechgar <tahamr08@gmail.com>
2023-01-13 16:04:41 +0100

    implement InMemoryRequestRepository to load, save & remove Authorization requests

---
+++ src/main/java/com/veinsmoke/myrhbackend/security/InMemoryRequestRepository.java
@@ -0,0 +1,40 @@
+package com.veinsmoke.myrhbackend.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InMemoryRequestRepository implements AuthorizationRequestRepository <OAuth2AuthorizationRequest>{
+
+    private final Map<String, OAuth2AuthorizationRequest> cache = new HashMap<>();
+
+    @Override
+    public OAuth2AuthorizationRequest loadAuthorizationRequest(HttpServletRequest request) {
+
+        String state = request.getParameter("state");
+        if(state != null ){
+            return removeAuthorizationRequest(request, null);
+        }
+        return null;
+    }
+
+    @Override
+    public void saveAuthorizationRequest(OAuth2AuthorizationRequest authorizationRequest, HttpServletRequest request, HttpServletResponse response) {
+        String state = authorizationRequest.getState();
+        cache.put( state , authorizationRequest);
+    }
+
+    @Override
+    public OAuth2AuthorizationRequest removeAuthorizationRequest(HttpServletRequest request, HttpServletResponse response) {
+
+        String state = request.getParameter("state");
+        if(state != null ){
+            return cache.remove(state);
+        }
+        return null;
+    }
+}
