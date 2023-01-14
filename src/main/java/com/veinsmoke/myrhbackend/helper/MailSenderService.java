1b6cf610f6e36d99381bfba45daeba5451a27bce
TahaLechgar <tahamr08@gmail.com>
2023-01-14 23:49:51 +0100

    add MailSenderService to send a simple email message

---
+++ src/main/java/com/veinsmoke/myrhbackend/helper/MailSenderService.java
@@ -0,0 +1,32 @@
+package com.veinsmoke.myrhbackend.helper;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+@PropertySource("classpath:application.yaml")
+public class MailSenderService {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    public void send(String to, String subject, String text) {
+        log.info("Sending email from: {}", from);
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+    }
+}
