3088d5f6cefb3b07b631d3a7bd7cba6c6421d3ef
TahaLechgar <tahamr08@gmail.com>
2023-01-14 23:29:53 +0100

    add CompanySize enum

---
+++ src/main/java/com/veinsmoke/myrhbackend/enums/CompanySize.java
@@ -0,0 +1,11 @@
+package com.veinsmoke.myrhbackend.enums;
+
+public enum CompanySize {
+
+    MICRO,      //(1, 10),
+    SMALL,      //(11, 50),
+    MEDIUM,     //(51, 250),
+    LARGE,      //(251, 1000),
+    HUGE        //(1001, 10000);
+
+}
