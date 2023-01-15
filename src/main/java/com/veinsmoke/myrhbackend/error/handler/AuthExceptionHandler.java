14b942f7bce2e744f768773a91683d822e02ac10
TahaLechgar <tahamr08@gmail.com>
2023-01-15 04:06:35 +0100

    add RegisterFailedException handler

--- src/main/java/com/veinsmoke/myrhbackend/error/handler/AuthExceptionHandler.java
+++ src/main/java/com/veinsmoke/myrhbackend/error/handler/AuthExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.veinsmoke.myrhbackend.error.handler;
 
 import com.veinsmoke.myrhbackend.error.exception.auth.LoginFailedException;
-import com.veinsmoke.myrhbackend.model.response.ErrorMessage;
+import com.veinsmoke.myrhbackend.error.exception.auth.RegisterFailedException;
+import com.veinsmoke.myrhbackend.error.exception.auth.VerificationFailedException;
+import com.veinsmoke.myrhbackend.model.response.ResponseMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,11 +15,22 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class AuthExceptionHandler {
 
     @ExceptionHandler(LoginFailedException.class)
-    public ResponseEntity<ErrorMessage> loginFailed(LoginFailedException exception) {
+    public ResponseEntity<ResponseMessage> loginFailed(LoginFailedException exception) {
         return ResponseEntity
                 .status(HttpStatus.FORBIDDEN)
-                .body(new ErrorMessage(HttpStatus.FORBIDDEN, exception.getMessage()));
+                .body(new ResponseMessage(HttpStatus.FORBIDDEN, exception.getMessage()));
     }
+
+    @ExceptionHandler(RegisterFailedException.class)
+    public ResponseEntity<ResponseMessage> registerFailed(RegisterFailedException exception) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(new ResponseMessage(HttpStatus.CONFLICT, exception.getMessage()));
+    }
+
+
+
+
 
 }
 
